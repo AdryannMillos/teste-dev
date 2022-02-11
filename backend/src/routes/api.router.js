9260d266fb06cb29b93cfa33158980a1f02f630e
AdryannMillos <81263284+AdryannMillos@users.noreply.github.com>
2022-02-11 09:35:52 -0300

    Delete backend directory

--- backend/src/routes/api.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bookController = require("../controllers/bookController.js");
-
-router.post("/book/create", bookController.createBook);
-
-
-module.exports = router;
